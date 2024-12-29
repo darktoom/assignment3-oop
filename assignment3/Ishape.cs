249a6f4de0f11d520a70d465e59157fcd5078776
darktoom <darktoom789@gmail.com>
2024-12-29 21:51:26 +0200

    Add project files.

---
+++ assignment3/Ishape.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment3
+{
+    public interface IShape
+    {
+        double Area { get; }
+        void DisplayShapeInfo();
+    }
+    public interface ICircle : IShape
+    {
+        double Radius { get; set; }
+
+        public interface IRectangle : IShape
+        {
+            double Length { get; set; }
+            double Width { get; set; }
+        }
+        public class Circle : ICircle
+        {
+            public double Radius { get; set; }  
+
+            public double Area => Math.PI * Radius * Radius; 
+
+            public void DisplayShapeInfo()
+            {
+                Console.WriteLine($"Circle with Radius: {Radius} and Area: {Area}");
+            }
+        }
+
+        public class Rectangle : IRectangle
+        {
+            public double Length { get; set; }  
+            public double Width { get; set; }  
+
+            public double Area => Length * Width;  
+
+            public void DisplayShapeInfo()
+            {
+                Console.WriteLine($"Rectangle with Length: {Length}, Width: {Width}, and Area: {Area}");
+            }
+        }
+
+
+    }
+
+
+
+
+}
---
+++ assignment3/Program.cs
@@ -0,0 +1,46 @@
+﻿using static assignment3.ICircle;
+
+namespace assignment3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ///1-b) To define a blueprint for a class
+            ///2-a) private
+            ///3-b) No
+            ///4-b) Yes, interfaces can inherit from multiple interfaces
+            ///5-d) implements
+            ///6-b) No
+            ///7-b) No, all members are implicitly public
+            ///8- a) To hide the interface members from outside access
+            ///9-b) No, interfaces cannot have constructors
+            ///10-c) By separating interface names with commas
+
+            ICircle circle = new Circle
+            {
+                Radius = 5
+            };
+
+            
+            circle.DisplayShapeInfo(); 
+
+            
+            IRectangle rectangle = new Rectangle
+            {
+                Length = 4,
+                Width = 6
+            };
+
+           
+            rectangle.DisplayShapeInfo();  
+        }
+
+
+
+
+
+
+    }
+    }
+
